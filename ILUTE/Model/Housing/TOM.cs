c3a788f98cfd249e56f69fda2a431f3815322804
mart2100 <martinezcharlie2020@gmail.com>
2025-06-02 13:31:39 -0400

    changes

--- ILUTE/Model/Housing/TOM.cs
+++ ILUTE/Model/Housing/TOM.cs
@@ -14,10 +14,38 @@ using XTMF;
 
 namespace TMG.Ilute.Model.Housing
 {
-	public class TOM
-	{
-		public TOM()
-		{
-		}
-	}
+    public class SellerBehaviorModel
+    {
+        // This determines three possible ways a home can leave the market. (1) Sold (2) Withdrawn and (3) Relisted
+        public enum Outcome { Sold, Withdrawn, Relisted }
+
+        public Outcome EvaluateOutcome(Dwelling dwelling, float askingPrice, int monthsOnMarket, bool isRelisted)
+        {
+            // Compute probabilities for each outcome using logit model
+            float pSold;
+            float pWithdraw;
+            float pRelist;
+
+        // Sample one outcome using those probabilities
+        float rand = Rand.NextFloat();
+            if (rand < pSold) return Outcome.Sold;
+            if (rand < pSold + pWithdraw) return Outcome.Withdrawn;
+            return Outcome.Relisted;
+        }
+
+        public float GetSurvivalProbability(Dwelling dwelling, int monthsOnMarket)
+        {
+            // Use a hazard function (e.g., exponential or Weibull)
+            float lambda = ComputeHazardRate(dwelling);
+            return (float)Math.Exp(-lambda * monthsOnMarket);
+        }
+
+        private float ComputeHazardRate(Dwelling dwelling)
+        {
+            // Use covariates: price, garage, etc.
+            // λ = exp(Xβ)
+            return MathF.Exp(...);
+        }
+    }
+
 }
