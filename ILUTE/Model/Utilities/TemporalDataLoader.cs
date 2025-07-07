4da15432f4d5d1f431536973c158e88d6242ac81
mart2100 <martinezcharlie2020@gmail.com>
2025-07-07 10:01:46 -0400

    updates

--- ILUTE/Model/Utilities/TemporalDataLoader.cs
+++ ILUTE/Model/Utilities/TemporalDataLoader.cs
@@ -25,120 +25,119 @@ using System.Threading.Tasks;
 using TMG.Input;
 using XTMF;
 
-namespace TMG.Ilute.Model.Utilities
-{
-    [ModuleInformation(Description = @"This module is designed to load in information for ILUTE in the format (Year/(year*12+month)) TAB (Data).
+namespace TMG.Ilute.Model.Utilities;
+
+[ModuleInformation(Description = @"This module is designed to load in information for ILUTE in the format (Year/(year*12+month)) TAB (Data).
     When using the module as a data source everything is converted into months.")]
-    public class TemporalDataLoader : IDataSource<SparseArray<float>>
+public class TemporalDataLoader : IDataSource<SparseArray<float>>
+{
+    [RootModule]
+    public MultiYearTravelDemandModel Root;
+
+    private SparseArray<float> _data;
+
+    [SubModelInformation(Required = true, Description = "The location to load the temporal data from.")]
+    public FileLocation LoadFrom;
+
+    [RunParameter("Headers", false, "Does the data file contain headers?")]
+    public bool Headers;
+
+    [RunParameter("Ignore Data Outside of Simulation", true, "Set this to true to ignore data outside of the model's time-frame. Data past the end of the simulation stops the read so it can be used if the period is extended.")]
+    public bool IgnoreDataOutsideOfSimulation;
+
+
+    [RunParameter("Data", false, "Data is monthly")]
+    public bool MonthlyData;
+
+    public string Name { get; set; }
+
+    public float Progress { get; set; }
+
+    public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
+
+    public bool Loaded { get; set; }
+
+
+    public bool RuntimeValidation(ref string error)
     {
-        [RootModule]
-        public MultiYearTravelDemandModel Root;
+        return true;
+    }
 
-        private SparseArray<float> _data;
+    public SparseArray<float> GiveData()
+    {
+        return _data;
+    }
 
-        [SubModelInformation(Required = true, Description = "The location to load the temporal data from.")]
-        public FileLocation LoadFrom;
+    private SparseArray<float> CreateBlankSparseArrayOfMonthData()
+    {
+        var startMonth = Root.StartYear * 12;
+        var endMonth = startMonth + Root.NumberOfYears * 12;
+        return new SparseArray<float>(new SparseIndexing() { Indexes = new SparseSet[] { new SparseSet() { Start = startMonth, Stop = endMonth - 1 } } }); ;
+    }
 
-        [RunParameter("Headers", false, "Does the data file contain headers?")]
-        public bool Headers;
-
-        [RunParameter("Ignore Data Outside of Simulation", true, "Set this to true to ignore data outside of the model's time-frame. Data past the end of the simulation stops the read so it can be used if the period is extended.")]
-        public bool IgnoreDataOutsideOfSimulation;
-
-
-        [RunParameter("Data", false, "Data is monthly")]
-        public bool MonthlyData;
-
-        public string Name { get; set; }
-
-        public float Progress { get; set; }
-
-        public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
-
-        public bool Loaded { get; set; }
-
-
-        public bool RuntimeValidation(ref string error)
+    public void LoadData()
+    {
+        var data = CreateBlankSparseArrayOfMonthData();
+        var startMonth = data.GetSparseIndex(0);
+        var endMonth = startMonth + Root.NumberOfYears * 12;
+        var flatData = data.GetFlatData();
+        using (CsvReader reader = new CsvReader(LoadFrom))
         {
-            return true;
-        }
-
-        public SparseArray<float> GiveData()
-        {
-            return _data;
-        }
-
-        private SparseArray<float> CreateBlankSparseArrayOfMonthData()
-        {
-            var startMonth = Root.StartYear * 12;
-            var endMonth = startMonth + Root.NumberOfYears * 12;
-            return new SparseArray<float>(new SparseIndexing() { Indexes = new SparseSet[] { new SparseSet() { Start = startMonth, Stop = endMonth - 1 } } }); ;
-        }
-
-        public void LoadData()
-        {
-            var data = CreateBlankSparseArrayOfMonthData();
-            var startMonth = data.GetSparseIndex(0);
-            var endMonth = startMonth + Root.NumberOfYears * 12;
-            var flatData = data.GetFlatData();
-            using (CsvReader reader = new CsvReader(LoadFrom))
+            int columns;
+            if (Headers)
             {
-                int columns;
-                if (Headers)
+                reader.LoadLine();
+            }
+            while (reader.LoadLine(out columns))
+            {
+                if (columns >= 2)
                 {
-                    reader.LoadLine();
-                }
-                while (reader.LoadLine(out columns))
-                {
-                    if (columns >= 2)
+                    bool year = false;
+                    int time;
+                    float entry;
+                    reader.Get(out time, 0);
+                    reader.Get(out entry, 1);
+                    if (time < startMonth)
                     {
-                        bool year = false;
-                        int time;
-                        float entry;
-                        reader.Get(out time, 0);
-                        reader.Get(out entry, 1);
-                        if (time < startMonth)
+                        // convert year to month
+                        time = time * 12;
+                        year = true;
+                    }
+                    if (time < startMonth || time >= endMonth)
+                    {
+                        if(IgnoreDataOutsideOfSimulation)
                         {
-                            // convert year to month
-                            time = time * 12;
-                            year = true;
-                        }
-                        if (time < startMonth || time >= endMonth)
-                        {
-                            if(IgnoreDataOutsideOfSimulation)
+                            if (time >= endMonth)
                             {
-                                if (time >= endMonth)
-                                {
-                                    break;
-                                }
-
-                                continue;
-                            }
-                            throw new XTMFRuntimeException(this, $"While loading data in '{Name}' we came across a month = '{time}' that isn't in the model's time-frame.");
-                        }
-                        if (year)
-                        {
-                            for (int i = 0; i < 12; i++)
-                            {
-                                flatData[time - startMonth + i] = entry;
+                                break;
                             }
 
+                            continue;
                         }
-                        else
+                        throw new XTMFRuntimeException(this, $"While loading data in '{Name}' we came across a month = '{time}' that isn't in the model's time-frame.");
+                    }
+                    if (year)
+                    {
+                        for (int i = 0; i < 12; i++)
                         {
-                            flatData[time - startMonth] = entry;
+                            flatData[time - startMonth + i] = entry;
                         }
+
+                    }
+                    else
+                    {
+                        flatData[time - startMonth] = entry;
                     }
                 }
             }
-            _data = data;
-            Loaded = true;
         }
+        _data = data;
+        Loaded = true;
+    }
 
-        public void UnloadData()
-        {
-            _data = null;
-            Loaded = false;
-        }
+    public void UnloadData()
+    {
+        _data = null;
+        Loaded = false;
     }
 }
